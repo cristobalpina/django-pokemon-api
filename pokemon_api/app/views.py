e9e19c4fc2182ec585004325f5a09b5b0a542fec
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-01-18 18:59:16 -0300

    :construction: Create pokemons function

--- pokemon_api/app/views.py
+++ pokemon_api/app/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import HttpResponse
 
 def index(request):
     return HttpResponse("Hello Pokemon API")
+
+
+def pokemons(request):
+    if request.method == 'GET':
+        return HttpResponse('Request sent with GET method')
+
+    if request.method == 'POST':
+        pass
+    
