de96e654b7eb66505fa21a96e5f373320cc9c97f
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-01-18 19:14:59 -0300

    :sparkles: Add create pokemon feature

--- pokemon_api/app/views.py
+++ pokemon_api/app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse
-
+import json
+from .models import *
 
 def index(request):
     return HttpResponse("Hello Pokemon API")
@@ -10,5 +11,13 @@ def pokemons(request):
         return HttpResponse('Request sent with GET method')
 
     if request.method == 'POST':
-        pass
-    
+        try:
+            data = json.loads(request.body)
+            pokemon = Pokemon.objects.create(
+                name=data['name'],
+                pokemon_type=data['pokemon_type'],
+                ability=data['ability'],
+            )
+            return HttpResponse(f"Added pokemon {data['name']} with ID {pokemon.id}")
+        except:
+            return HttpResponse('There was an error while adding the pokemon to the database')
