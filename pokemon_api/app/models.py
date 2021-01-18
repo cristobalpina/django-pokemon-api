35676772cd3ff9c256bfaa03643bcea01992f7e2
JMAVJ <josemiguel.velasquezj@gmail.com>
2021-01-18 18:49:03 -0300

    :construction: Add models and make migrations

--- pokemon_api/app/models.py
+++ pokemon_api/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=45)
+    pokemon_type = models.CharField(max_length=45)
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=45)
+    pokemon = models.ForeignKey(Pokemon, related_name='abilities',on_delete=models.CASCADE)
